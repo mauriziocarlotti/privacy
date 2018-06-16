9eb97a85972460c652683fb016c71613c6866c49
Maurizio Carlotti <maurizio.carlotti@satelsrl.it>
2018-06-16 08:01:35 +0200

    Creazione setup progetto

---
+++ privacyback/src/main/java/it/satelsrl/privacyback/App.java
@@ -0,0 +1,13 @@
+package it.satelsrl.privacyback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
